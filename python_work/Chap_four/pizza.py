989efc38e2d88af121a49051124b4ae3b7e002b2
Harvey Swick <marshalldickey@gmail.com>
2020-04-22 13:44:48 -0700

    Added instructions and fist steps of code.

--- python_work/Chap_four/pizza.py
+++ python_work/Chap_four/pizza.py
@@ -1,1 +1,21 @@
-#4-1
+
+#4-1 Think of at least three kinds of your favorite pizza. Store these
+#pizza names in a list, and then use a for loop to print the name of 
+#each pizza
+#	-Modify your for loop to print a sentence using the name os the
+#	pizza instead of printing just the name of the pizza. For each 
+#	pizza you should have one line of output containing a simple 
+#	statement like 'I like pepperoni pizza.'
+#	-Add a line at the end of your program, outside the for loop, that
+#	consist of the three or more lines about  the kinds of pizza you 
+#	like and then an additonal sentence, such as 'I really love pizza!'
+
+pizzas = ['supreme', 'pepperoni', 'pineapple']
+
+for pizza in pizzas:
+	print(f'The {pizza} pizza is here.')
+
+print("""
+Gah, I love pizza just, so much. So much pizza I love, Just, Gah. I love
+Just pizza... MAN! DO I LOVE IT!
+""")
