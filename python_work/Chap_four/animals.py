704acd60885b98cdcb78f0874818fc03c0c2a52f
Harvey Swick <62694173+Disunito@users.noreply.github.com>
2020-04-22 17:34:54 -0700

    Forgot to save last change.

---
+++ python_work/Chap_four/animals.py
@@ -0,0 +1,13 @@
+#4-2 Think of at least three different animals that have a common
+#charactaristic. Store the names of these animals in a list, and then use a for
+#loop to print out the name of each animal.
+#   -modify your program to print a staement abouyt each animal, such as a dog
+#   would make a great pet.
+#   -Add a line at the end of your program stating what these animals have in
+#   common. you could print a sentence such as Any of these animals would make
+#   a great pet!
+
+animals = ['bunny', 'dragon', 'octopus']
+
+for animal in animals:
+    print(animals)
