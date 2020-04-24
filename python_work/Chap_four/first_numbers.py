93260c5143697267148c958cfd4e12c82561d77f
Marshall Kerr Dickey <Disunito@johnny-5.lan>
2020-04-24 15:05:24 -0700

    "A small program using range() and list() function to express numbers"

---
+++ python_work/Chap_four/first_numbers.py
@@ -0,0 +1,11 @@
+#Test of range() function
+for value in range(1, 5):
+    print(value)
+for value in range(1, 6):
+    print(value)
+for value in range(6):
+    print(value)
+
+#Using rnage() to make a list of numbers
+numbers = list(range(1, 6))
+print(numbers)
