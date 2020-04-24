7293bd870c4b93cfea693cc8924e53ab6b523045
Harvey Swick <pi@Wintermute.lan>
2020-04-24 00:02:41 -0700

    Adusted for loop to display each animals name.

--- python_work/Chap_four/animals.py
+++ python_work/Chap_four/animals.py
@@ -1,13 +1,13 @@
 #4-2 Think of at least three different animals that have a common
-#charactaristic. Store the names of these animals in a list, and then use a for
-#loop to print out the name of each animal.
-#   -modify your program to print a statement about each animal, such as a dog
-#   would make a great pet.
-#   -Add a line at the end of your program stating what these animals have in
-#   common. you could print a sentence such as Any of these animals would make
-#   a great pet!
+#charactaristic. Store the names of these animals in a list, and then 
+#use a for loop to print out the name of each animal.
+#   -modify your program to print a statement about each animal, 
+#    such as a dog would make a great pet.
+#   -Add a line at the end of your program stating what these animals 
+#    have in common. you could print a sentence such as Any of these 
+#    animals would make a great pet!
 
 animals = ['bunnies', 'dragons', 'octopus']
 
 for animal in animals:
-    print(f"I find {animal[0]} can be easily sexualized.")
+    print(f"I find {animal} can be easily be written into fantasy.")
