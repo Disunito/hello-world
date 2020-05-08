a673ae3ed34cbe1bdab2a7c929ba2a4e64b33246
Marshall Kerr Dickey <Disunito@johnny-5.lan>
2020-05-08 16:44:26 -0700

    "begining of Chapter six and dictionaries"

---
+++ python_work/Chap_six/alien.py
@@ -0,0 +1,18 @@
+alien_0 = {}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+print(alien_0)
+
+alien_color = alien_0['color']
+print(f'the alien is {alien_color}.')
+print(alien_0)
+
+alien_0['color'] = 'yellow'
+print(f'Now it is {alien_color}.')
+print(alien_0)
