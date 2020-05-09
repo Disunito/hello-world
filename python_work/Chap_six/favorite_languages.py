97cb9eb79b4e730e072c7ca2a9ae39bca844b201
Marshall Kerr Dickey <Disunito@johnny-5.lan>
2020-05-08 20:49:49 -0700

    "Updated fav_languages.py"

---
+++ python_work/Chap_six/favorite_languages.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+    'Jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}")
