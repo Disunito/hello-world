2f5726ea8b223d41ac908d81a27144135b7b971e
Marshall Kerr Dickey <Disunito@johnny-5.lan>
2020-05-08 21:20:52 -0700

    "finished 6-1"

---
+++ python_work/Chap_six/person.py
@@ -0,0 +1,14 @@
+#6-1 Use a dictionary to store information about a person you know.
+#Store their first, Last name, age, and CIty they live in. you should
+#Print each peice of information stored.
+
+audrey_c = {
+    'first_name': 'audrey',
+    'last_name': 'coffins',
+    'age': 35,
+    'city': 'los angeles'
+}
+
+print(audrey_c['first_name'].title(), audrey_c['last_name'].title())
+print(audrey_c['age'])
+print(audrey_c['city'].title())
