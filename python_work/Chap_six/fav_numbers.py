efdd4ba0d2fdd217f24bf345e00c2c8589ec2653
Marshall Kerr Dickey <Disunito@Johnny-5.attlocal.net>
2020-05-10 20:28:04 -0700

    "New changers for chapter six"

---
+++ python_work/Chap_six/fav_numbers.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'kayla': 42,
+    'cristina': 7,
+    'marshall': 13,
+    'thom': 0,
+    'satan': 666,
+}
+
+print(f"Kayla's favorite number is {favorite_numbers['kayla']}")
+print(f"Cristina's favorite number is {favorite_numbers['cristina']}")
+print(f"Marshall's favorite number is {favorite_numbers['marshall']}")
+print(f"Thom's favorite number is {favorite_numbers['thom']}")
+print(f"Satan's favorite number is {favorite_numbers['satan']}")
---
+++ python_work/Chap_six/glossary.py
@@ -0,0 +1,6 @@
+#6-3 Think of 5 programming words you've learned and store their names
+#as keys and definition as values. Print each in a neat format.
+
+glossary = {
+
+}
