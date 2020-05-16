0897453a6871e138d20d6f763ceddd5f93cc5c62
Harvey Swick <pi@Wintermute.lan>
2020-05-16 10:25:22 -0700

    Trying .items() and .keys() methods

--- python_work/Chap_six/favorite_languages.py
+++ python_work/Chap_six/favorite_languages.py
@@ -2,8 +2,18 @@ favorite_languages = {
     'Jen': 'python',
     'sarah': 'c',
     'edward': 'ruby',
-    'phil': 'python',
+    'phil': 'java',
 }
 
-language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}")
+for person, language in favorite_languages.items():
+    print(f"{person.title()}'s favorite language is {language.title()}")
+
+friends = ['phil', 'sarah']
+for person in favorite_languages:
+    print(f"Hi {person.title()}")
+    if person in friends:
+        language = favorite_languages[person].title()
+        print(f"\t{person.title()}, I see you love {language}!")
+
+if 'erin' not in favorite_languages:
+    print("Erin, please take our poll")
