91e0cf0c3da2c271e5e0ddfe08c6fb2d24de6cb8
Marshall Kerr Dickey <Disunito@Johnny-5.local>
2020-05-31 07:55:18 -0700

    "Added notes and set() At the end of program"

--- python_work/Chap_six/favorite_languages.py
+++ python_work/Chap_six/favorite_languages.py
@@ -2,12 +2,14 @@ favorite_languages = {
     'jen': 'python',
     'sarah': 'c',
     'edward': 'ruby',
-    'phil': 'java',
+    'phil': 'python',
 }
 
+# I.E. for key, value in Dictionary
 for person, language in favorite_languages.items():
     print(f"{person.title()}'s favorite language is {language.title()}")
 
+#Used a list to call out two different keys from the rest of the Dictionary
 friends = ['phil', 'sarah']
 for person in favorite_languages:
     print(f"Hi {person.title()}")
@@ -18,6 +20,11 @@ for person in favorite_languages:
 if 'erin' not in favorite_languages:
     print("Erin, please take our poll")
 
-#why is this not sorting the keys?
+#Used sorted() to Thank each person in alphabetical order
 for person in sorted(favorite_languages.keys()):
     print(f"Thanks for taking our poll, {person.title()}")
+
+#Added set() to sort any duplicate languages mentioned
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
