7d2b48dc9206bd38052e71562e9a14de06d923fa
Marshall Kerr Dickey <Disunito@Johnny-5.local>
2020-05-31 09:46:39 -0700

    "Added Terms and definitions as well as a print"

--- python_work/Chap_six/glossary.py
+++ python_work/Chap_six/glossary.py
@@ -2,5 +2,12 @@
 #as keys and definition as values. Print each in a neat format.
 
 glossary = {
-
+    'dictionary': 'a set with key, value pairs, where each key is unique',
+    'set': 'an unordered collection with no duplicate elements',
+    'tuple': 'a number of values separated by commas',
+    'list comprehension': 'a concise way to create lists',
+    'list': 'a mutable, or changeable, ordered sequence of elements',
 }
+
+for word, definition in glossary.items():
+    print(f"{word}:\n\t{definition}")
